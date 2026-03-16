94c7434165397b657968dbb97b9d5a4fba5806aa
kirtheeesh <kirtheeswarangrm@gmail.com>
2026-03-16 19:14:54 +0530

    Allow seamless local and production database connections

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = process.env.DATABASE_URL ? new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 }) : new Pool({
     user: 'postgres',
     host: 'localhost',
